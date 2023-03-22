8030fa880f606534ff465eaeac52d3acaee3187b
Rafał <120196269+rafalspa@users.noreply.github.com>
2023-03-22 12:27:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -33,88 +33,111 @@ function drawMap() {
     }
 };
 
-// Function that detects collision with walls and lets player move
-function playerMovement() {
 const player = document.querySelector(".player");
 const allWalls = document.querySelectorAll(".wall");
 let newPosX = 192;
 let newPosY = 144;
 
-window.addEventListener('keydown', function (e) {
-    if (e.key === 'd') {
-    let wallPosX;
-    let wallPosY;
-    let canGoRight = 1;
-     for (let i=0; i < allWalls.length; i++){
-        wallPosX = allWalls[i].getBoundingClientRect().left;
-        wallPosY = allWalls[i].getBoundingClientRect().top;
-        if ((newPosX) === wallPosX - 32 && newPosY === wallPosY) {
-            canGoRight = 0;
-           }
-     }
-     if (canGoRight === 1) {
-        newPosX += 32;
-        player.style.left = newPosX + "px";
-        console.log(newPosX);
-        console.log(player.style.left);
-     }
+// Function that detects collision with walls and lets player move
+function playerMovement() {
+    window.addEventListener('keydown', function (e) {
+        if (e.key === 'd') {
+        let wallPosX;
+        let wallPosY;
+        let canGoRight = 1;
+         for (let i=0; i < allWalls.length; i++){
+            wallPosX = allWalls[i].getBoundingClientRect().left;
+            wallPosY = allWalls[i].getBoundingClientRect().top;
+            if ((newPosX) === wallPosX - 32 && newPosY === wallPosY) {
+                canGoRight = 0;
+               }
+         }
+         if (canGoRight === 1) {
+            newPosX += 32;
+            player.style.left = newPosX + "px";
+            console.log(newPosX);
+            console.log(player.style.left);
+            checkPoint();
+         }
+        }
+       }, false);
+      
+       window.addEventListener('keydown', function (e) {
+        if (e.key === 'a') {
+        let wallPosX;
+        let wallPosY;
+        let canGoLeft = 1;
+         for (let i=0; i < allWalls.length; i++){
+            wallPosX = allWalls[i].getBoundingClientRect().right;
+            wallPosY = allWalls[i].getBoundingClientRect().top;
+            if ((newPosX) === wallPosX && newPosY === wallPosY) {
+                canGoLeft = 0;
+               }
+         }
+         if (canGoLeft === 1) {
+            newPosX -= 32;
+            player.style.left = newPosX + "px";
+            checkPoint();
+         }
+        }
+       }, false);
+    
+       window.addEventListener('keydown', function (e) {
+        if (e.key === 'w') {
+        let wallPosX;
+        let wallPosY;
+        let canGoUp = 1;
+         for (let i=0; i < allWalls.length; i++){
+            wallPosX = allWalls[i].getBoundingClientRect().right;
+            wallPosY = allWalls[i].getBoundingClientRect().top;
+            if ((newPosX) === wallPosX-32 && newPosY === wallPosY+32) {
+                canGoUp = 0;
+               }
+         }
+         if (canGoUp === 1) {
+            newPosY -= 32;
+            player.style.top = newPosY + "px";
+            checkPoint();
+         }
+        }
+       }, false);
+    
+       window.addEventListener('keydown', function (e) {
+        if (e.key === 's') {
+        let wallPosX;
+        let wallPosY;
+        let canGoDown = 1;
+         for (let i=0; i < allWalls.length; i++){
+            wallPosX = allWalls[i].getBoundingClientRect().left;
+            wallPosY = allWalls[i].getBoundingClientRect().top;
+            if ((newPosX) === wallPosX && newPosY === wallPosY-32) {
+                canGoDown = 0;
+               }
+         }
+         if (canGoDown === 1) {
+            newPosY += 32;
+            player.style.top = newPosY + "px";
+            checkPoint();
+         }
+        }
+       }, false);
     }
-   }, false);
-  
-   window.addEventListener('keydown', function (e) {
-    if (e.key === 'a') {
-    let wallPosX;
-    let wallPosY;
-    let canGoLeft = 1;
-     for (let i=0; i < allWalls.length; i++){
-        wallPosX = allWalls[i].getBoundingClientRect().right;
-        wallPosY = allWalls[i].getBoundingClientRect().top;
-        if ((newPosX) === wallPosX && newPosY === wallPosY) {
-            canGoLeft = 0;
-           }
-     }
-     if (canGoLeft === 1) {
-        newPosX -= 32;
-        player.style.left = newPosX + "px";
-     }
-    }
-   }, false);
 
-   window.addEventListener('keydown', function (e) {
-    if (e.key === 'w') {
-    let wallPosX;
-    let wallPosY;
-    let canGoUp = 1;
-     for (let i=0; i < allWalls.length; i++){
-        wallPosX = allWalls[i].getBoundingClientRect().right;
-        wallPosY = allWalls[i].getBoundingClientRect().top;
-        if ((newPosX) === wallPosX-32 && newPosY === wallPosY+32) {
-            canGoUp = 0;
-           }
-     }
-     if (canGoUp === 1) {
-        newPosY -= 32;
-        player.style.top = newPosY + "px";
-     }
-    }
-   }, false);
+//Function that checks collision with a point and changes the score
+function checkPoint () {
+    const allPoints = document.querySelectorAll(".point");
+    let pointPosX;
+    let pointPosY;
+    let score = document.querySelector("#scoreCount");
+    let currentScore = parseInt(score.innerHTML);
 
-   window.addEventListener('keydown', function (e) {
-    if (e.key === 's') {
-    let wallPosX;
-    let wallPosY;
-    let canGoDown = 1;
-     for (let i=0; i < allWalls.length; i++){
-        wallPosX = allWalls[i].getBoundingClientRect().left;
-        wallPosY = allWalls[i].getBoundingClientRect().top;
-        if ((newPosX) === wallPosX && newPosY === wallPosY-32) {
-            canGoDown = 0;
+    for (let i=0; i < allPoints.length; i++){
+        pointPosX = allPoints[i].getBoundingClientRect().left;
+        pointPosY = allPoints[i].getBoundingClientRect().top;
+        if (newPosX === (pointPosX-12) && newPosY === (pointPosY-12)) {
+            allPoints[i].remove();
+            currentScore += 1;
+            score.innerHTML = currentScore;
            }
      }
-     if (canGoDown === 1) {
-        newPosY += 32;
-        player.style.top = newPosY + "px";
-     }
-    }
-   }, false);
 }
